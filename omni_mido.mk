bf27422e87c478a5d5e5a0cfd4d2fa2f921899ef
Exodus <zjemcikolege@protonmail.com>
2018-07-30 01:12:32 +0200

    Oreo LUL

--- omni_mido.mk
+++ omni_mido.mk
@@ -26,10 +26,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit language packages
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-PRODUCT_PACKAGES += \
-	charger_res_images \
-	charger
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
 PRODUCT_NAME := omni_mido
